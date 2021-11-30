4e8244827b39fbb7c3528b155a4076d4b6706def
mickykro <mickykroapps@gmail.com>
2021-12-01 00:30:28 +0200

    match tables and show dialog

---
+++ restaurant/src/Design/dialog.js
@@ -0,0 +1,55 @@
+import  React,{useEffect, useState} from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
+
+
+export default function ResponsiveDialog({ freeSeats, number , start, openDialog , setOpen }) {
+    const theme = useTheme();
+    const [innerOpen, setInnerOpen] = useState(openDialog);
+    const dialogTitle = freeSeats > 0 ? 'available table' : 'occupied table';
+    const dialogText = freeSeats > 0 ? `table number ${number} started at ${new Date(start).toLocaleDateString('en-US', { hour: 'numeric', minute: 'numeric', hour12: false })}`: 
+    `table number ${number} is available and has ${freeSeats} empty seats`;
+
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+    useEffect(()=>{
+        setInnerOpen(openDialog);
+    },[])
+
+    const handleClose = () => {
+        setInnerOpen(false);
+    };
+
+
+    return (
+        <div>
+            <Dialog
+                fullScreen={fullScreen}
+                open={innerOpen}
+                onClose={handleClose}
+                aria-labelledby="responsive-dialog-title"
+            >
+                <DialogTitle id="responsive-dialog-title">
+                    {dialogTitle}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                       {dialogText}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={handleClose}>
+                        close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
