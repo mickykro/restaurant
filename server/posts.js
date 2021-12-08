655de1c3f5e5b279091574e99361a838fe489237
mickykroapps@gmail.com <mickykroapps@gmail.com>
2021-12-08 16:27:03 +0200

    remove prints

--- server/posts.js
+++ server/posts.js
@@ -5,20 +5,17 @@ const {orders, floor, completedPath} = require('./utils');
 
 
 exports.getOrders = async (req, res, next)=>{
-console.log('get orders: ',JSON.parse(orders));
     res.json(JSON.parse(orders))
-    // res.get(orders);
+    
 }
 exports.getFloor = async (req, res, next)=>{
-    console.log(floor);
    res.json(JSON.parse(floor));
-    // res.end(floor);
 }
 
 
 exports.addPosts =  async (req, res) => {
         const dataToAppend = req.body.json;
-        fs.w
+        
      fs.writeFile(completedPath,JSON.stringify({data:dataToAppend, dateAdded: Date.now()}),(err)=>{
          console.log(err);
      });
