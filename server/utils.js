a850dfc0f181607614b70dd1843365145368d75e
mickykro <mickykroapps@gmail.com>
2021-12-16 15:34:10 +0200

    fix jsons paths

--- server/utils.js
+++ server/utils.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const ordersPath = path.resolve('C:/Users/Dell/Desktop/resume/poalim_project/restaurant-1/server/jsons', 'orders.json');
-const floorPath = path.resolve('C:/Users/Dell/Desktop/resume/poalim_project/restaurant-1/server/jsons', 'floor.json');
-exports.completedPath = path.resolve('C:/Users/Dell/Desktop/resume/poalim_project/restaurant-1/server/jsons', 'completed_orders.json')
+const ordersPath = path.resolve('jsons', 'orders.json');
+const floorPath = path.resolve('jsons', 'floor.json');
+exports.completedPath = path.resolve('jsons', 'completed_orders.json')
 
 exports.orders = getJson(ordersPath);
 exports.floor = getJson(floorPath);
